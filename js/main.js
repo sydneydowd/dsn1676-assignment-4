59fcac92a8fb760cc69f436599bddc8594394269
sydneydowd <sydneydowd@Sydneys-MacBook-Pro.local>
2015-02-12 15:49:43 -0500

    Connected the js.main in index.html
    added in the information into main.js using var groups with full name

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var blueberries = {
+  fullName: 'vaccinium corymbosum',
+  growsOnShrub: true,
+  energy: 240,
+  carbohydrates: 14.49,
+  protein: 0.74
+};
+console.log(blueberries.fullName);
+
+var grapes = {
+  fullName: 'Vitis vinifera',
+  growsOnShrub: false,
+  energy: 288,
+  carbohydrates: 18.1,
+  protein: 0.72
+};
+console.log(grapes.fullName);
+
+var redcurrent = {
+  fullName: 'Ribes rubrum',
+  growsOnShrub: true,
+  energy: 234,
+  carbohydrates: 13.8,
+  protein:  1.4
+};
+console.log(redcurrent.fullName);
